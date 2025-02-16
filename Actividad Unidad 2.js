bdd3e388c7a5ddebe0192cf9e77693874e256532
romario_lam <romariolam91@gmail.com>
2025-02-16 12:02:21 -0600

    primer commit

---
+++ Actividad Unidad 2.js
@@ -0,0 +1,51 @@
+var Dia = prompt("Ingrese su dia de Nacimiento")
+var Mes = prompt("Ingrese su mes de Nacimineto")
+
+if(Mes ==3 && Dia >=21) alert("entonces es Aries")
+;
+if(Mes ==4 && Dia <=21) alert("entonces es Aries")
+;
+if(Mes ==4 && Dia >=21) alert("entonces es Tauro")
+;
+if(Mes ==5 && Dia <=20) alert("entonces es Tauro")
+;
+if(Mes ==5 && Dia >=21) alert("entonces es Geminis")
+;
+if(Mes ==6 && Dia <=20) alert("entonces es Geminis")
+;
+if(Mes ==6 && Dia >=21) alert("entonces es Cancer")
+;
+if(Mes ==7 && Dia <=22) alert("entonces es Cancer")
+;
+if(Mes ==7 && Dia >=23) alert("entonces es Leo")
+;
+if(Mes ==8 && Dia <=23) alert("entonces es Leo")
+;
+if(Mes ==8 && Dia >=24) alert("entonces es Virgo")
+;
+if(Mes ==9 && Dia <=22) alert("entonces es Virgo")
+;
+if(Mes ==9 && Dia >=23) alert("entonces es Libra")
+;
+if(Mes ==10 && Dia <=23) alert("entonces es Libra")
+;
+if(Mes ==10 && Dia >=24) alert("entonces es Escorpio")
+;
+if(Mes ==11 && Dia <=22) alert("entonces es Escorpio")
+;
+if(Mes ==11 && Dia >=23) alert("entonces es Sagitario")
+;
+if(Mes ==12 && Dia <=21) alert("entonces es Sagitario")
+;
+if(Mes ==12 && Dia >=22) alert("entonces es Capricornio")
+;
+if(Mes ==1 && Dia <=20) alert("entonces es Capricornio")
+;
+if(Mes ==1 && Dia >=21) alert("entonces es Acuario")
+;
+if(Mes ==2 && Dia <=19) alert("entonces es Acuario")
+;
+if(Mes ==2 && Dia >=20) alert("entonces es Piscis")
+;
+if(Mes ==4 && Dia <=20) alert("entonces es Piscis")
+;
